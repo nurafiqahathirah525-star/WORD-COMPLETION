13245272f191bfa69f4810587c88a97e4e252d3c
nurafiqahathirah525-star <nurafiqahathirah525@gmail.com>
2026-09-20 23:56:52 +0800

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'english-champion-v1';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request).catch(() => {
+                return caches.match('./index.html');
+            });
+        })
+    );
+});
